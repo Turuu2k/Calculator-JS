db7c37edcc0268db8472aa1ef76d775529730053
= <=>
2024-08-06 09:46:01 +0800

    ok

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const firstEl = document.querySelector("#first");
-const secondEl = document.querySelector("#second");
-const lastEl = document.querySelector("#last");
+const displayEl = document.querySelector("#display")
 
 const minusBtn = document.querySelector("#minusBtn");
 const plusBtn = document.querySelector("#plusBtn");
 const multiBtn = document.querySelector("#multiBtn");
 const divideBtn = document.querySelector("#divideBtn");
 const clearBtn = document.querySelector("#clearBtn");
+const equalBtn = document.querySelector("#equalBtn");
+const oneBtn = document.querySelector("#oneBtn");
+const twoBtn = document.querySelector("#twoBtn");
+const threeBtn = document.querySelector("#threeBtn");
+const fourBtn = document.querySelector("#fourBtn");
+const fiveBtn = document.querySelector("#fiveBtn");
+const sixBtn = document.querySelector("#sixBtn");
+const sevenBtn = document.querySelector("#sevenBtn");
+const eightBtn = document.querySelector("#eightBtn");
+const nineBtn = document.querySelector("#nineBtn");
 
 minusBtn.addEventListener("click", () => {
     let value = firstEl.value;
